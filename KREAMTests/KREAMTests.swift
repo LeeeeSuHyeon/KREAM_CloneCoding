8dd2666a91ac87897c1b46985f00a817b531b76e
suhyeon lee <tngus0673@naver.com>
2024-11-06 18:24:30 +0900

    first commit

---
+++ KREAMTests/KREAMTests.swift
@@ -0,0 +1,17 @@
+//
+//  KREAMTests.swift
+//  KREAMTests
+//
+//  Created by 이수현 on 11/6/24.
+//
+
+import Testing
+@testable import KREAM
+
+struct KREAMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
